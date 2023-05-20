73ebfc91f0df319a9d291a5c8a7ff9bf43609c98
Patsanan Suksiri <patsanas.suksiri@gmail.com>
2023-05-20 16:50:19 +0700

    add condition

--- app_streamlit.py
+++ app_streamlit.py
@@ -39,97 +39,98 @@ def main():
         st.write("New DataFrame:")
         st.write(new_df)
 
-        # Prepare subplots
-        st.write("Show Data Distribution:")
-        # fig, axs = plt.subplots(1, min(4, len(selected_columns)), figsize=(20, 5))  # Adjust figure size here
+        if new_df:
+            # Prepare subplots
+            st.write("Show Data Distribution:")
+            # fig, axs = plt.subplots(1, min(4, len(selected_columns)), figsize=(20, 5))  # Adjust figure size here
 
-        # # Plotting histograms of each selected column
-        # for i, column in enumerate(selected_columns[:4]):  # Limiting to first 4 columns
-        #     axs[i].hist(new_df[column])
-        #     axs[i].set_title(f"Histogram for {column}")
+            # # Plotting histograms of each selected column
+            # for i, column in enumerate(selected_columns[:4]):  # Limiting to first 4 columns
+            #     axs[i].hist(new_df[column])
+            #     axs[i].set_title(f"Histogram for {column}")
 
-        # # Show the plot
-        # st.pyplot(fig)
-        # Determine the number of rows for the subplot grid
-        num_cols = len(selected_columns)
-        num_rows = num_cols // 4
-        if num_cols % 4: 
-            num_rows += 1  # Add an extra row if columns don't divide evenly by 4
+            # # Show the plot
+            # st.pyplot(fig)
+            # Determine the number of rows for the subplot grid
+            num_cols = len(selected_columns)
+            num_rows = num_cols // 4
+            if num_cols % 4: 
+                num_rows += 1  # Add an extra row if columns don't divide evenly by 4
 
-        # Prepare subplots
-        fig, axs = plt.subplots(num_rows, 4, figsize=(20, 5 * num_rows))  # Adjust figure size here
-        axs = axs.ravel()  # Flatten the array to iterate easily
+            # Prepare subplots
+            fig, axs = plt.subplots(num_rows, 4, figsize=(20, 5 * num_rows))  # Adjust figure size here
+            axs = axs.ravel()  # Flatten the array to iterate easily
 
-        # Plotting histograms of each selected column
-        for i, column in enumerate(selected_columns):
-            axs[i].hist(new_df[column])
-            axs[i].set_title(f"Histogram for {column}")
+            # Plotting histograms of each selected column
+            for i, column in enumerate(selected_columns):
+                axs[i].hist(new_df[column])
+                axs[i].set_title(f"Histogram for {column}")
 
-        # If less than num_rows*4 plots, remove the empty subplots
-        if num_cols < num_rows * 4:
-            for i in range(num_cols, num_rows * 4):
-                fig.delaxes(axs[i])
-        # Show the plot
-        st.pyplot(fig)
+            # If less than num_rows*4 plots, remove the empty subplots
+            if num_cols < num_rows * 4:
+                for i in range(num_cols, num_rows * 4):
+                    fig.delaxes(axs[i])
+            # Show the plot
+            st.pyplot(fig)
 
-        # Calculate the number of scatter plots
-        num_plots = len(selected_columns) * (len(selected_columns) - 1) // 2
+            # Calculate the number of scatter plots
+            num_plots = len(selected_columns) * (len(selected_columns) - 1) // 2
 
-        # Calculate the number of rows needed for the subplot grid
-        num_rows = num_plots // 4
-        if num_plots % 4: 
-            num_rows += 1  # Add an extra row if the plots don't divide evenly by 4
+            # Calculate the number of rows needed for the subplot grid
+            num_rows = num_plots // 4
+            if num_plots % 4: 
+                num_rows += 1  # Add an extra row if the plots don't divide evenly by 4
 
-        # Prepare subplots
-        fig, axs = plt.subplots(num_rows, 4, figsize=(20, 5 * num_rows))
-        axs = axs.ravel()  # Flatten the array to iterate easily
+            # Prepare subplots
+            fig, axs = plt.subplots(num_rows, 4, figsize=(20, 5 * num_rows))
+            axs = axs.ravel()  # Flatten the array to iterate easily
 
-        # Plotting scatter plots of each pair of selected columns
-        i = 0
-        for pair in itertools.combinations(selected_columns, 2):
-            axs[i].scatter(new_df[pair[0]], new_df[pair[1]])
-            axs[i].set_title(f"Scatter plot: {pair[0]} vs {pair[1]}")
-            i += 1
+            # Plotting scatter plots of each pair of selected columns
+            i = 0
+            for pair in itertools.combinations(selected_columns, 2):
+                axs[i].scatter(new_df[pair[0]], new_df[pair[1]])
+                axs[i].set_title(f"Scatter plot: {pair[0]} vs {pair[1]}")
+                i += 1
 
-        # If less than num_rows*4 plots, remove the empty subplots
-        if num_plots < num_rows * 4:
-            for i in range(num_plots, num_rows * 4):
-                fig.delaxes(axs[i])
+            # If less than num_rows*4 plots, remove the empty subplots
+            if num_plots < num_rows * 4:
+                for i in range(num_plots, num_rows * 4):
+                    fig.delaxes(axs[i])
 
-        # Show the plot
-        st.pyplot(fig)
+            # Show the plot
+            st.pyplot(fig)
 
 
 
-        models = {
-        "Linear Regression": LinearRegression(),
-        "Decision Tree": DecisionTreeRegressor(),
-        "Random Forest": RandomForestRegressor(),
-}
-        # Select target variable
-        st.write("Develop Machine Learning Model :")
-        target = st.selectbox("Select target variable", new_df.columns)
+            models = {
+            "Linear Regression": LinearRegression(),
+            "Decision Tree": DecisionTreeRegressor(),
+            "Random Forest": RandomForestRegressor(),
+    }
+            # Select target variable
+            st.write("Develop Machine Learning Model :")
+            target = st.selectbox("Select target variable", new_df.columns)
 
-        X = new_df.drop(target, axis=1)
-        y = new_df[target]
+            X = new_df.drop(target, axis=1)
+            y = new_df[target]
 
-        # Split the data
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            # Split the data
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Train and evaluate models
-        st.write("Model Evaluation:")
-        best_model_name = ""
-        best_model_score = 0.0
-        for name, model in models.items():
-            model.fit(X_train, y_train)
-            cv_score = cross_val_score(model, X_train, y_train, cv=5).mean()
-            st.write(f"{name} cross-validation score: {cv_score}")
-            if cv_score > best_model_score:
-                best_model_name = name
-                best_model_score = cv_score
+            # Train and evaluate models
+            st.write("Model Evaluation:")
+            best_model_name = ""
+            best_model_score = 0.0
+            for name, model in models.items():
+                model.fit(X_train, y_train)
+                cv_score = cross_val_score(model, X_train, y_train, cv=5).mean()
+                st.write(f"{name} cross-validation score: {cv_score}")
+                if cv_score > best_model_score:
+                    best_model_name = name
+                    best_model_score = cv_score
 
-        st.write(f"Best Model: {best_model_name} with score: {best_model_score}")
-                
+            st.write(f"Best Model: {best_model_name} with score: {best_model_score}")
+                    
             
 
 if __name__ == "__main__":
