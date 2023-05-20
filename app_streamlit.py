c59cabdfb4ea297245b25ce20013d2e8b354f2b5
Patsanan Suksiri <patsanas.suksiri@gmail.com>
2023-05-20 18:32:29 +0700

    debug

--- app_streamlit.py
+++ app_streamlit.py
@@ -71,33 +71,33 @@ def Auto_ML():
 
             df = df.copy()
 
-            for col in new_df.columns:
-                if is_object_dtype(new_df[col]):
+            for col in df.columns:
+                if is_object_dtype(df[col]):
                     try:
-                        new_df[col] = pd.to_datetime(new_df[col])
+                        df[col] = pd.to_datetime(df[col])
                     except Exception:
                         pass
 
-                if is_datetime64_any_dtype(new_df[col]):
-                    new_df[col] = new_df[col].dt.tz_localize(None)
+                if is_datetime64_any_dtype(df[col]):
+                    df[col] = df[col].dt.tz_localize(None)
 
             modification_container = st.container()
 
             with modification_container:
-                to_filter_columns = st.multiselect("Filter dataframe on", new_df.columns)
+                to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
                 for column in to_filter_columns:
                     left, right = st.columns((1, 20))
                     # Treat columns with < 10 unique values as categorical
-                    if is_categorical_dtype(new_df[column]) or new_df[column].nunique() < 10:
+                    if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
                         user_cat_input = right.multiselect(
                             f"Values for {column}",
-                            new_df[column].unique(),
-                            default=list(new_df[column].unique()),
+                            df[column].unique(),
+                            default=list(df[column].unique()),
                         )
-                        new_df = new_df[new_df[column].isin(user_cat_input)]
-                    elif is_numeric_dtype(new_df[column]):
-                        _min = float(new_df[column].min())
-                        _max = float(new_df[column].max())
+                        df = df[df[column].isin(user_cat_input)]
+                    elif is_numeric_dtype(df[column]):
+                        _min = float(df[column].min())
+                        _max = float(df[column].max())
                         step = (_max - _min) / 100
                         user_num_input = right.slider(
                             f"Values for {column}",
@@ -106,27 +106,27 @@ def Auto_ML():
                             value=(_min, _max),
                             step=step,
                         )
-                        new_df = new_df[new_df[column].between(*user_num_input)]
-                    elif is_datetime64_any_dtype(new_df[column]):
+                        df = df[df[column].between(*user_num_input)]
+                    elif is_datetime64_any_dtype(df[column]):
                         user_date_input = right.date_input(
                             f"Values for {column}",
                             value=(
-                                new_df[column].min(),
-                                new_df[column].max(),
+                                df[column].min(),
+                                df[column].max(),
                             ),
                         )
                         if len(user_date_input) == 2:
                             user_date_input = tuple(map(pd.to_datetime, user_date_input))
                             start_date, end_date = user_date_input
-                            new_df = new_df.loc[new_df[column].between(start_date, end_date)]
+                            df = df.loc[df[column].between(start_date, end_date)]
                     else:
                         user_text_input = right.text_input(
                             f"Substring or regex in {column}",
                         )
                         if user_text_input:
-                            new_df = new_df[new_df[column].astype(str).str.contains(user_text_input)]
+                            df = df[df[column].astype(str).str.contains(user_text_input)]
 
-            return new_df
+            return df
         
         new_df = filter_dataframe(new_df)
         
